36177f1036e972d1e65930a3f8b1312da6342a8a
Cam <>
2018-07-11 14:22:28 -0700

    auth on react

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,14 +1,19 @@
 import * as types from '../constants/actionTypes';
 
-export default (state = [], action) => {
+const initialState = {
+  isUserLoggedIn: false
+};
+
+export default (state = initialState, action) => {
   let users;
   switch (action.type) {
     case types.GET_USERS:
       users = action.users;
-      return users;
+      return state;
     case types.IS_LOGGED_OUT:
-      users = action.users;
-      return users;
+      return !state.isUserLoggedIn;
+    case types.LOG_IN_USER:
+      return Object.assign({}, state, {isUserLoggedIn: action.payload});
     default:
       return state;
   }
