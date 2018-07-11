9ee53c3ead6f869b084c0529487c0c00840fe090
Adam Modras <25adam@gmail.com>
2018-07-11 10:52:19 -0700

    working on Postgres Models

---
+++ server/models/user.js
@@ -0,0 +1,58 @@
+const bcrypt = require('bcrypt-nodejs');
+
+module.exports = (sequelize, DataTypes) => {
+
+  const User = sequelize.define('User', {
+    _id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      field: 'userid',
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+        len: { args: 3 }
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: { args: 3 }
+      },
+      field: 'password',
+    },
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    // freezeTableName: true
+  });
+
+  User.associate = (models) => {
+    models.User.belongsTo(models.Board, {
+      foreignKey: {
+        allowNull: false,
+      },
+      as: 'board',
+      onDelete: 'SET NULL'
+    })
+  };
+
+  User.beforeCreate((model, options) => {
+    return new Promise((resolve, reject) => {
+      model.password = bcrypt.hashSync(model.password, bcrypt.genSaltSync(8), null)
+      return resolve(model, options);
+    });
+  })
+
+  // Alternate approach: https://stackoverflow.com/questions/31427566/sequelize-create-model-with-beforecreate-hook
+  
+  User.methods.validPassword = (password) => {
+    return bcrypt.compareSync(password, this.local.password);
+  };
+
+  return User;
+}
