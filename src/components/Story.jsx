10534c26de65e18832fcb0b89239f948637e47b6
Clariz Mariano <clariz.mariano@gmail.com>
2018-07-11 18:56:03 -0700

    initial pimpification

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -2,35 +2,40 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { updateStory, deleteStory } from '../actions/stories.js';
+// style={{
+//   border: '1px solid gray',
+//   padding: '4px 4px',
+//   marginTop: '2px',
+//   marginBottom: '2px',
+//   marginRight: '2px',
+//   marginLeft: '2px',
+//   height: '100px',
+//   width: '90%',
+//   boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)',
 
+// }}
 const Task = props => {
   const order = ['todo', 'inProgress', 'testing', 'done'];
   return (
-    <div
-      style={{
-        border: '2.5px solid black',
-        padding: '4px 4px',
-        backgroundColor: props.task.done ? 'green' : 'red',
-        marginTop: '2px',
-        marginBottom: '2px',
-      }}
-    >
-      <button
-        className="delete button_clear--small"
-        onClick={() => props.deleteTask(props.task._id)}
-      >
-        X
-      </button>
-      <p>{props.name}</p>
-      <span
-        onClick={() => {
+    <div   className="taskbox">
+         <button
+          className="delete button_clear--small"
+          onClick={() => this.props.deleteTask(this.props.task._id)}
+        >
+          X
+        </button>
+        <p>{props.name} </p>
+
+        <div className="task-status">
+          <span  onClick={() => {
           let { done } = props.task;
           console.log('​done', done);
           props.updateStory(props.task, { done: !done });
         }}
-      >
-        Status
-      </span>
+        style={{
+             backgroundColor: props.task.done ? 'green' : 'red',
+        }}> Status </span>
+        </div>
     </div>
   );
 };
